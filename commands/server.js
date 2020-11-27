9d33b70cf804855379b5b91403fb7b67b0954961
Leon Henning <LeonHenning@mein.gmx>
2020-11-27 16:41:52 +0100

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -9,6 +9,9 @@ module.exports = {
         .addField(name="Server IP", value="modattack.apexmc.co", inline=true)
         .addField(name="Server Version", value="Forge 1.15.2", inline=true)
         .addField(name="Spieler Slots", value="100", inline=false)
+	.addField(name="Server Zeiten", value="Immer", inline=false)
+	.addField(name="Server Backup", value="Alle 2 Stunden", inline=true)
+	.addField(name="Server neustart", value="Immer um 00:00 Uhr, 5 Minuten vorher gibt es eine info", inline=true)
         message.channel.send(embed);
     }
 }
